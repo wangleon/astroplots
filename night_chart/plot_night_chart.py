578b3a727ae6605b050a61489bb826cd8c04eb87
Liang Wang <wang.leon@gmail.com>
2018-09-14 01:42:32 +0200

    add RA lines (also local sidereal time) in night chart

--- night_chart/plot_night_chart.py
+++ night_chart/plot_night_chart.py
@@ -9,11 +9,13 @@ import matplotlib.colors as mcolors
 
 def correct_ut(dt):
     # correct UT to Central European Time in 2019
+    # returns +2 or +1 (CET - UT)
+    # so CET = CET + correct_ut(dt)
     if dt > datetime.datetime(2019, 3, 31, 2, 0, 0) and \
        dt < datetime.datetime(2019, 10,27, 3, 0, 0):
-        return dt + datetime.timedelta(hours=2)
+        return datetime.timedelta(hours=2)
     else:
-        return dt + datetime.timedelta(hours=1)
+        return datetime.timedelta(hours=1)
 
 def main():
 
@@ -47,8 +49,10 @@ def main():
         myplace.date = dt
         t1 = myplace.previous_setting(sun, use_center=True)
         t2 = myplace.next_rising(sun, use_center=True)
-        dt1 = correct_ut(t1.datetime()) - dt
-        dt2 = correct_ut(t2.datetime()) - dt
+        ut_offset1 = correct_ut(t1.datetime())
+        ut_offset2 = correct_ut(t2.datetime())
+        dt1 = t1.datetime() + ut_offset1 - dt
+        dt2 = t2.datetime() + ut_offset2 - dt
         dt1_lst.append(date0 + dt1)
         dt2_lst.append(date0 + dt2)
     ax.plot(dt1_lst, datenums, '-', color='C3', alpha=0.5)
@@ -61,8 +65,10 @@ def main():
         myplace.date = dt
         t1 = myplace.previous_setting(ep.Sun(), use_center=True)
         t2 = myplace.next_rising(ep.Sun(), use_center=True)
-        dt1 = correct_ut(t1.datetime()) - dt
-        dt2 = correct_ut(t2.datetime()) - dt
+        ut_offset1 = correct_ut(t1.datetime())
+        ut_offset2 = correct_ut(t2.datetime())
+        dt1 = t1.datetime() + ut_offset1 - dt
+        dt2 = t2.datetime() + ut_offset2 - dt
         dt1_lst.append(date0 + dt1)
         dt2_lst.append(date0 + dt2)
     ax.fill_betweenx(datenums, dt1_lst, dt2_lst, color='C0', alpha=0.2, lw=0)
@@ -74,8 +80,10 @@ def main():
         myplace.date = dt
         t1 = myplace.previous_setting(ep.Sun(), use_center=True)
         t2 = myplace.next_rising(ep.Sun(), use_center=True)
-        dt1 = correct_ut(t1.datetime()) - dt
-        dt2 = correct_ut(t2.datetime()) - dt
+        ut_offset1 = correct_ut(t1.datetime())
+        ut_offset2 = correct_ut(t2.datetime())
+        dt1 = t1.datetime() + ut_offset1 - dt
+        dt2 = t2.datetime() + ut_offset2 - dt
         dt1_lst.append(date0 + dt1)
         dt2_lst.append(date0 + dt2)
     ax.fill_betweenx(datenums, dt1_lst, dt2_lst, color='C0', alpha=0.2, lw=0)
@@ -104,7 +112,8 @@ def main():
     for t in fullmoon_lst:
         myplace.date = t + datetime.timedelta(hours=5)
         transit_dt = myplace.previous_transit(moon).datetime()
-        delta_dt = correct_ut(transit_dt) - datetime.datetime(transit_dt.year,
+        ut_offset = correct_ut(transit_dt)
+        delta_dt = transit_dt + ut_offset - datetime.datetime(transit_dt.year,
                                                   transit_dt.month,
                                                   transit_dt.day, 0,0,0)
         if delta_dt.total_seconds()>12*3600:
@@ -114,6 +123,62 @@ def main():
 
     # plot the full moons
     ax.scatter(transit_lst, fullmoon_lst, color='C1', alpha=0.6, s=30, lw=0)
+
+    # plot RA lines
+    for ra in np.arange(0, 24, 3):
+        # generate a fake star with (RA, Dec) = (ra, 0.0)
+        eq = SkyCoord(ra=ra*15, dec=0., frame='icrs', unit='deg')
+        string = 'NONAME,f|M|g2,%s,%s,5.0,2000'%(
+                eq.ra.to_string(sep=':'), eq.dec.to_string(sep=':'))
+        star = ep.readdb(string)
+        transit_lst = []
+        real_transit_lst = []
+        dt_lst = []
+
+        transit_data = []
+        prev_transit = None
+        for dt in datetime_lst:
+            myplace.date = dt + datetime.timedelta(hours=12)
+            star.compute(myplace)
+
+            real_transit_dt = myplace.previous_transit(star).datetime()
+            delta_dt = real_transit_dt - dt
+            transit = date0 + delta_dt
+
+            if prev_transit is None or \
+                (transit - prev_transit).total_seconds() < 12*3600:
+                transit_lst.append(transit)
+                real_transit_lst.append(real_transit_dt)
+                dt_lst.append(dt)
+            else:
+                item = (transit_lst, dt_lst, real_transit_lst)
+                transit_data.append(item)
+                transit_lst = []
+                real_transit_lst = []
+                dt_lst = []
+                prev_transit = None
+            prev_transit = transit
+        if len(dt_lst)>0:
+            item = (transit_lst, dt_lst, real_transit_lst)
+            transit_data.append(item)
+
+        # plot
+        for transit_lst, dt_lst, real_transit_lst in transit_data:
+            ut_offset_lst = [correct_ut(dt) for dt in real_transit_lst]
+            newtransit_lst = [t1+to for t1, to in zip(transit_lst, ut_offset_lst)]
+            ax.plot(newtransit_lst, dt_lst, '--', color='k', alpha=0.2, lw=1)
+
+            # add texts of RA in hour on the top
+            if dt_lst[0] == datetime_lst[0] and x1 < newtransit_lst[0] < x2:
+                ax.text(newtransit_lst[0],
+                        dt_lst[0] + datetime.timedelta(days=6),
+                        '%02dh'%ra, color='k', alpha=0.6)
+
+            # add texts of RA in hour on the bottom
+            if dt_lst[-1] == datetime_lst[-1] and x1 < newtransit_lst[-1] < x2:
+                ax.text(newtransit_lst[-1],
+                        dt_lst[-1] - datetime.timedelta(days=2),
+                        '%02dh'%ra, color='k', alpha=0.6)
 
     # set x and y axis to date
     ax.xaxis_date()
