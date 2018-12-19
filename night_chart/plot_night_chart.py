0cbbbacd36215047a81536609ac74b9bc14ab993
Liang Wang <wang.leon@gmail.com>
2018-12-19 01:58:21 +0100

    update night chart

--- night_chart/plot_night_chart.py
+++ night_chart/plot_night_chart.py
@@ -23,8 +23,9 @@ def plot_nightchart(longitude, latitude, elevation, correct_ut, xlabel,
 
     # set range of plot dates
     date0 = datetime.datetime(2019,1,1,0,0,0)
-    datetime_lst = [date0 + datetime.timedelta(days=i) for i in range(365)]
-    datenums = mdates.date2num(datetime_lst)
+    local_datetime_lst = [date0 + datetime.timedelta(hours=2*i) for i in range(365*12)]
+    #local_datetime_lst = [date0 + datetime.timedelta(days=i) for i in range(365)]
+    datenums = mdates.date2num(local_datetime_lst)
 
     # initialize figure
     fig = plt.figure(dpi=150,figsize=(10,14))
@@ -34,193 +35,208 @@ def plot_nightchart(longitude, latitude, elevation, correct_ut, xlabel,
     x1 = date0 - datetime.timedelta(hours=9)
     x2 = date0 + datetime.timedelta(hours=9)
 
-    # plot sun rise and sun set time
-    myplace.horizon=0
-    dt1_lst, dt2_lst = [], []
-    for idt, dt in enumerate(datetime_lst):
-        myplace.date = dt
-        t1 = myplace.previous_setting(sun, use_center=True)
-        t2 = myplace.next_rising(sun, use_center=True)
-        ut_offset1 = correct_ut(t1.datetime())
-        ut_offset2 = correct_ut(t2.datetime())
-        dt1 = t1.datetime() + ut_offset1 - dt
-        dt2 = t2.datetime() + ut_offset2 - dt
+    plot_sun  = True
+    plot_twg  = True
+    plot_moon = True
+    plot_ra   = True
 
-        # if dt2 > 1 day, move date to 1 day forward
-        if dt2 > datetime.timedelta(days=1):
-            myplace.date = dt - datetime.timedelta(days=1)
+    if plot_sun:
+        # plot sun rise and sun set time
+        myplace.horizon=0
+        dt1_lst, dt2_lst = [], []
+        for idt, dt in enumerate(local_datetime_lst):
+            ut_offset = correct_ut(dt)
+            ut = dt - ut_offset
+            myplace.date = ut
+            t1 = myplace.previous_setting(sun, use_center=True)
             t2 = myplace.next_rising(sun, use_center=True)
-            ut_offset2 = correct_ut(t2.datetime())
-            dt2 = t2.datetime() + ut_offset2 - dt
+            midnight = datetime.datetime.combine(dt.date(), datetime.time.min)
+            dt1 = t1.datetime() + ut_offset - midnight
+            dt2 = t2.datetime() + ut_offset - midnight
 
-        dt1_lst.append(date0 + dt1)
-        dt2_lst.append(date0 + dt2)
-    ax.plot(dt1_lst, datenums, '-', color='C3', alpha=0.5)
-    ax.plot(dt2_lst, datenums, '-', color='C3', alpha=0.5)
+            if dt1 > datetime.timedelta(hours=12):
+                dt1 -= datetime.timedelta(hours=24)
+            if dt2 > datetime.timedelta(hours=12):
+                dt2 -= datetime.timedelta(hours=24)
 
-    # plot civilized twilight time
-    myplace.horizon='-6'
-    dt1_lst, dt2_lst = [], []
-    for idt, dt in enumerate(datetime_lst):
-        myplace.date = dt
-        t1 = myplace.previous_setting(sun, use_center=True)
-        t2 = myplace.next_rising(sun, use_center=True)
-        ut_offset1 = correct_ut(t1.datetime())
-        ut_offset2 = correct_ut(t2.datetime())
-        dt1 = t1.datetime() + ut_offset1 - dt
-        dt2 = t2.datetime() + ut_offset2 - dt
+            dt1_lst.append(date0 + dt1)
+            dt2_lst.append(date0 + dt2)
+        artist_sun, = ax.plot(dt1_lst, datenums, '-', color='C3', alpha=0.5)
+        ax.plot(dt2_lst, datenums, '-', color='C3', alpha=0.5)
 
-        # if dt2 > 1 day, move date to 1 day forward
-        if dt2 > datetime.timedelta(days=1):
-            myplace.date = dt - datetime.timedelta(days=1)
+    if plot_twg:
+        # plot civilized twilight time
+        myplace.horizon='-6'
+        dt1_lst, dt2_lst = [], []
+        for idt, dt in enumerate(local_datetime_lst):
+            ut_offset = correct_ut(dt)
+            ut = dt - ut_offset
+            myplace.date = ut
+            t1 = myplace.previous_setting(sun, use_center=True)
             t2 = myplace.next_rising(sun, use_center=True)
-            ut_offset2 = correct_ut(t2.datetime())
-            dt2 = t2.datetime() + ut_offset2 - dt
+            midnight = datetime.datetime.combine(dt.date(), datetime.time.min)
+            dt1 = t1.datetime() + ut_offset - midnight
+            dt2 = t2.datetime() + ut_offset - midnight
 
-        dt1_lst.append(date0 + dt1)
-        dt2_lst.append(date0 + dt2)
-    ax.fill_betweenx(datenums, dt1_lst, dt2_lst, color='C0', alpha=0.2, lw=0)
+            if dt1 > datetime.timedelta(hours=12):
+                dt1 -= datetime.timedelta(hours=24)
+            if dt2 > datetime.timedelta(hours=12):
+                dt2 -= datetime.timedelta(hours=24)
 
-    # plot astronomical twilight time
-    myplace.horizon='-18'
-    dt1_lst, dt2_lst = [], []
-    for idt, dt in enumerate(datetime_lst):
-        myplace.date = dt
-        t1 = myplace.previous_setting(sun, use_center=True)
-        t2 = myplace.next_rising(sun, use_center=True)
-        ut_offset1 = correct_ut(t1.datetime())
-        ut_offset2 = correct_ut(t2.datetime())
-        dt1 = t1.datetime() + ut_offset1 - dt
-        dt2 = t2.datetime() + ut_offset2 - dt
+            dt1_lst.append(date0 + dt1)
+            dt2_lst.append(date0 + dt2)
+        artist_ctwg = ax.fill_betweenx(datenums, dt1_lst, dt2_lst, color='C0',
+                alpha=0.2, lw=0)
 
-        # if dt2 > 1 day, move date to 1 day forward
-        if dt2 > datetime.timedelta(days=1):
-            myplace.date = dt - datetime.timedelta(days=1)
+        # plot astronomical twilight time
+        myplace.horizon='-18'
+        dt1_lst, dt2_lst = [], []
+        for idt, dt in enumerate(local_datetime_lst):
+            ut_offset = correct_ut(dt)
+            ut = dt - ut_offset
+            myplace.date = ut
+            t1 = myplace.previous_setting(sun, use_center=True)
             t2 = myplace.next_rising(sun, use_center=True)
-            ut_offset2 = correct_ut(t2.datetime())
-            dt2 = t2.datetime() + ut_offset2 - dt
+            midnight = datetime.datetime.combine(dt.date(), datetime.time.min)
+            dt1 = t1.datetime() + ut_offset - midnight
+            dt2 = t2.datetime() + ut_offset - midnight
 
-        dt1_lst.append(date0 + dt1)
-        dt2_lst.append(date0 + dt2)
-    ax.fill_betweenx(datenums, dt1_lst, dt2_lst, color='C0', alpha=0.2, lw=0)
+            if dt1 > datetime.timedelta(hours=12):
+                dt1 -= datetime.timedelta(hours=24)
+            elif dt1 < datetime.timedelta(hours=-12):
+                dt1 += datetime.timedelta(hours=24)
 
-    # get times of full moons
-    fullmoon_lst = []
-    i = 0
-    while(True):
-        dt = date0 + datetime.timedelta(days=i)
-        myplace.date = dt
-        moon.compute(myplace)
-        if dt > datetime_lst[-1]:
-            break
-        t = ep.next_full_moon(dt)
-        found = False
-        for _t in fullmoon_lst:
-            deltat = _t - t.datetime()
-            if abs(deltat.total_seconds())<24*3600:
-                found = True
-        if not found and t.datetime()<datetime_lst[-1]:
-            fullmoon_lst.append(t.datetime())
-        i += 15
+            if dt2 > datetime.timedelta(hours=12):
+                dt2 -= datetime.timedelta(hours=24)
 
-    # get transit time of each full moon
-    transit_lst = []
-    for t in fullmoon_lst:
-        myplace.date = t + datetime.timedelta(hours=5)
-        transit_dt = myplace.previous_transit(moon).datetime()
-        ut_offset = correct_ut(transit_dt)
-        delta_dt = transit_dt + ut_offset - datetime.datetime(transit_dt.year,
-                                                  transit_dt.month,
-                                                  transit_dt.day, 0,0,0)
-        if delta_dt.total_seconds()>12*3600:
-            delta_dt = delta_dt - datetime.timedelta(hours=24)
+            dt1_lst.append(date0 + dt1)
+            dt2_lst.append(date0 + dt2)
+        ax.fill_betweenx(datenums, dt1_lst, dt2_lst, color='C0', alpha=0.2, lw=0)
 
-        transit_lst.append(date0 + delta_dt)
+        # plot a fake twilight artist
+        artist_atwg = ax.fill_betweenx(datenums-1e3,dt1_lst, dt2_lst,
+                color='C0', alpha=0.36, lw=0) 
 
-    # plot the full moons
-    ax.scatter(transit_lst, fullmoon_lst, color='C1', alpha=0.6, s=30, lw=0)
-
-    # plot RA lines
-    for ra in np.arange(0, 24, 3):
-        # generate a fake star with (RA, Dec) = (ra, 0.0)
-        eq = SkyCoord(ra=ra*15, dec=0., frame='icrs', unit='deg')
-        string = 'NONAME,f|M|g2,%s,%s,5.0,2000'%(
-                eq.ra.to_string(unit='h', sep=':'),
-                eq.dec.to_string(unit='deg',sep=':'))
-        star = ep.readdb(string)
+    if plot_moon:
+        # get times of full moons
+        fullmoon_lst = []
+        i = 0
+        while(True):
+            dt = date0 + datetime.timedelta(days=i)
+            if dt > local_datetime_lst[-1]:
+                break
+            myplace.date = dt
+            moon.compute(myplace)
+            t = ep.next_full_moon(dt)
+            found = False
+            for _t in fullmoon_lst:
+                deltat = _t - t.datetime()
+                if abs(deltat.total_seconds())<24*3600:
+                    found = True
+            if not found and t.datetime()<local_datetime_lst[-1]:
+                fullmoon_lst.append(t.datetime())
+            i += 15
+ 
+        # get transit time of each full moon
         transit_lst = []
-        real_transit_lst = []
-        dt_lst = []
+        for t in fullmoon_lst:
+            myplace.date = t + datetime.timedelta(hours=5)
+            transit_dt = myplace.previous_transit(moon).datetime()
+            ut_offset = correct_ut(transit_dt)
+            midnight = datetime.datetime.combine(transit_dt.date(), datetime.time.min)
+            #delta_dt = transit_dt + ut_offset - datetime.datetime(transit_dt.year,
+            #                                          transit_dt.month,
+            #                                          transit_dt.day, 0,0,0)
+            delta_dt = transit_dt + ut_offset - midnight
+            if delta_dt.total_seconds()>12*3600:
+                delta_dt -= datetime.timedelta(hours=24)
+ 
+            transit_lst.append(date0 + delta_dt)
+ 
+        # plot the full moons
+        artist_moon = ax.scatter(transit_lst, fullmoon_lst, color='C1', alpha=0.6,
+                s=30, lw=0)
 
-        transit_data = []
-        prev_transit = None
-        for dt in datetime_lst:
-            myplace.date = dt + datetime.timedelta(hours=12)
-            star.compute(myplace)
-
-            real_transit_dt = myplace.previous_transit(star).datetime()
-            delta_dt = real_transit_dt - dt
-            transit = date0 + delta_dt
-            if delta_dt + correct_ut(dt) > datetime.timedelta(hours=12):
-                transit = transit - datetime.timedelta(hours=24)
-
-            if prev_transit is None or \
-                abs((transit - prev_transit).total_seconds()) < 12*3600:
-                transit_lst.append(transit)
-                real_transit_lst.append(real_transit_dt)
-                dt_lst.append(dt)
-            else:
+    if plot_ra:
+        # plot RA lines
+        for ra in np.arange(0, 24, 3):
+            # generate a fake star with (RA, Dec) = (ra, 0.0)
+            eq = SkyCoord(ra=ra*15, dec=0., frame='icrs', unit='deg')
+            string = 'NONAME,f|M|g2,%s,%s,5.0,2000'%(
+                    eq.ra.to_string(unit='h', sep=':'),
+                    eq.dec.to_string(unit='deg',sep=':'))
+            star = ep.readdb(string)
+            transit_lst = []
+            real_transit_lst = []
+            dt_lst = []
+ 
+            transit_data = []
+            prev_transit = None
+            for dt in local_datetime_lst:
+                midnight = datetime.datetime.combine(dt.date(), datetime.time.min)
+                ut_offset = correct_ut(dt)
+                ut = dt - ut_offset
+                myplace.date = ut
+                star.compute(myplace)
+ 
+                real_transit_dt = myplace.previous_transit(star).datetime()
+                delta_dt = real_transit_dt + ut_offset - midnight
+                if delta_dt > datetime.timedelta(hours=12):
+                    delta_dt -= datetime.timedelta(hours=24)
+                elif delta_dt < datetime.timedelta(hours=-12):
+                    delta_dt += datetime.timedelta(hours=24)
+                transit = date0 + delta_dt
+ 
+                if prev_transit is None or \
+                    abs((transit - prev_transit).total_seconds()) < 12*3600:
+                    transit_lst.append(transit)
+                    real_transit_lst.append(real_transit_dt)
+                    dt_lst.append(dt)
+                else:
+                    item = (transit_lst, dt_lst, real_transit_lst)
+                    transit_data.append(item)
+                    # reset the list
+                    transit_lst = []
+                    real_transit_lst = []
+                    dt_lst = []
+                    prev_transit = None
+                prev_transit = transit
+            if len(dt_lst)>0:
                 item = (transit_lst, dt_lst, real_transit_lst)
                 transit_data.append(item)
-                # reset the list
-                transit_lst = []
-                real_transit_lst = []
-                dt_lst = []
-                prev_transit = None
-            prev_transit = transit
-        if len(dt_lst)>0:
-            item = (transit_lst, dt_lst, real_transit_lst)
-            transit_data.append(item)
-
-        # plot
-        for transit_lst, dt_lst, real_transit_lst in transit_data:
-            ut_offset_lst = [correct_ut(dt) for dt in real_transit_lst]
-
-            newtransit_lst = []
-            for t1, to in zip(transit_lst, ut_offset_lst):
-                newtransit_lst.append(t1+to)
-
-            ax.plot(newtransit_lst, dt_lst, '--', color='k', alpha=0.2, lw=1)
-            # for _t1, _t2 in zip(transit_lst, newtransit_lst):
-            #    print(ra, _t1, _t2)
-
-            # add texts of RA in hour on the top
-            if dt_lst[0] == datetime_lst[0] and x1 < newtransit_lst[0] < x2:
-                ax.text(newtransit_lst[0],
-                        dt_lst[0] + datetime.timedelta(days=6),
-                        '%02dh'%ra, color='k', alpha=0.6)
-
-            # add texts of RA in hour on the bottom
-            if dt_lst[-1] == datetime_lst[-1] and x1 < newtransit_lst[-1] < x2:
-                ax.text(newtransit_lst[-1],
-                        dt_lst[-1] - datetime.timedelta(days=2),
-                        '%02dh'%ra, color='k', alpha=0.6)
-
-            # add texts of RA in hour on the left
-            dist_lst = np.array([abs((x1-t).total_seconds()) for t in newtransit_lst])
-            if dist_lst.min() < 2*3600:
-                i = dist_lst.argmin()
-                if i>0 and i<len(dist_lst):
-                    ax.text(x1+datetime.timedelta(minutes=8), dt_lst[i],
-                        '%02dh'%ra, color='k', alpha=0.6)
-
-            # add texts of Dec in hour on the right
-            dist_lst = np.array([abs((x2-t).total_seconds()) for t in newtransit_lst])
-            if dist_lst.min() < 2*3600:
-                i = dist_lst.argmin()
-                if i>0 and i<len(dist_lst):
-                    ax.text(x2-datetime.timedelta(minutes=30), dt_lst[i],
-                        '%02dh'%ra, color='k', alpha=0.6)
+ 
+            # plot
+            for transit_lst, dt_lst, real_transit_lst in transit_data:
+ 
+                artist_ra, = ax.plot(transit_lst, dt_lst, '-', color='k', alpha=0.2, lw=0.8)
+ 
+                # add texts of RA in hour on the top
+                if dt_lst[0] == local_datetime_lst[0] and x1 < transit_lst[0] < x2:
+                    ax.text(transit_lst[0] - datetime.timedelta(minutes=15),
+                            dt_lst[0] + datetime.timedelta(days=6),
+                            '%02dh'%ra, color='k', alpha=0.6)
+ 
+                # add texts of RA in hour on the bottom
+                if dt_lst[-1] == local_datetime_lst[-1] and x1 < transit_lst[-1] < x2:
+                    ax.text(transit_lst[-1] - datetime.timedelta(minutes=15),
+                            dt_lst[-1] - datetime.timedelta(days=2),
+                            '%02dh'%ra, color='k', alpha=0.6)
+ 
+                # add texts of RA in hour on the left
+                dist_lst = np.array([abs((x1-t).total_seconds()) for t in transit_lst])
+                if dist_lst.min() < 2*3600:
+                    i = dist_lst.argmin()
+                    if i>0 and i<len(dist_lst):
+                        ax.text(x1+datetime.timedelta(minutes=8), dt_lst[i],
+                            '%02dh'%ra, color='k', alpha=0.6)
+ 
+                # add texts of Dec in hour on the right
+                dist_lst = np.array([abs((x2-t).total_seconds()) for t in transit_lst])
+                if dist_lst.min() < 2*3600:
+                    i = dist_lst.argmin()
+                    if i>0 and i<len(dist_lst):
+                        ax.text(x2-datetime.timedelta(minutes=40), dt_lst[i],
+                            '%02dh'%ra, color='k', alpha=0.6)
 
 
     # set x and y axis to date
@@ -240,10 +256,15 @@ def plot_nightchart(longitude, latitude, elevation, correct_ut, xlabel,
     ax.set_xlim(x1, x2)
     ax.set_ylim(datenums[-1], datenums[0])
 
-    ax.grid(True, ls='--', alpha=0.6, lw=1,  which='major')
-    ax.grid(True, ls='--', alpha=0.5, lw=0.5, which='minor')
+    ax.grid(True, ls=':', alpha=0.6, lw=1,  which='major')
+    ax.grid(True, ls=':', alpha=0.5, lw=0.5, which='minor')
     ax.set_xlabel(xlabel, fontsize=14)
     ax.set_ylabel('Date', fontsize=14)
+
+    ax.legend((artist_sun, artist_moon, artist_ctwg, artist_atwg, artist_ra),
+            ('Sunsets & Sunrises', 'Tranists of Full Moon', 'Civilized Twilight', 'Astronomical Twilight', 'Local Sidereal Time'),
+            loc = 'center left',
+            )
 
     fig.savefig(figname)
 
